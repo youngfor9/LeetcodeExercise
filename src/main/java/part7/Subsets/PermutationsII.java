944011ea05ef5d709b4a3324fd7f3909c40d97e2
youg_for9 <1287855967@qq.com>
2020-09-17 21:56:43 +0800

    双指针类型

---
+++ src/main/java/part7/Subsets/PermutationsII.java
@@ -0,0 +1,48 @@
+package part7.Subsets;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/permutations-ii/
+ * 给一个有重复元素的数组中，找出所有子集
+ * Input: [1,1,2]
+ * Output:
+ * [
+ * [1,1,2],
+ * [1,2,1],
+ * [2,1,1]
+ * ]
+ */
+public class PermutationsII {
+    @Test
+    public void init() {
+        int[] nums = {1,1,3};
+        List<List<Integer>> lists = permuteUnique(nums);
+        System.out.println(lists);
+    }
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, new boolean[nums.length]);
+        return list;
+    }
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, boolean [] used){
+        if(tempList.size() == nums.length){
+            list.add(new ArrayList<>(tempList));
+        } else{
+            for(int i = 0; i < nums.length; i++){
+                if(used[i] || i > 0 && nums[i] == nums[i-1] && !used[i - 1]) continue;
+                used[i] = true;
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums, used);
+                used[i] = false;
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}

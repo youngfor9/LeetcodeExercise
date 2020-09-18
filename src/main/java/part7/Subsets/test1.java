bf59e45bc4fa422c838c87903bb4eb4f126fb451
youg_for9 <1287855967@qq.com>
2020-09-18 22:14:42 +0800

    11

--- src/main/java/part7/Subsets/test1.java
+++ src/main/java/part7/Subsets/test1.java
@@ -2,7 +2,8 @@ package part7.Subsets;
 
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("删除");
+        int i =9;
+        System.out.println(i);
     }
 }
 
